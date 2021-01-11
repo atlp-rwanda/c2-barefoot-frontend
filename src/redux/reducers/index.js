885bcbbb04e0785595f438f061c96b354cbf79b4
inezabonte <inezabonte@gmail.com>
2021-01-11 09:41:24 +0200

    users page

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,7 +4,7 @@ import { signupRequestReducer } from './signupReducer'
 import {fetchLocationsReducer} from './locationsReducer'
 import { fetchAccommodationsReducer } from './accommodationsReducer'
 import { logoutReducer } from './logoutReducer';
-import { fetchUsersReducer } from './usersReducer'
+import { UsersReducer } from './usersReducer'
 import { RolesReducer } from './rolesReducer'
 import { createRoles } from './createRoleReducer'
 const reducers = combineReducers({
@@ -13,7 +13,7 @@ const reducers = combineReducers({
   signup: signupRequestReducer,
   fetchLocations: fetchLocationsReducer,
   fetchAccommodations: fetchAccommodationsReducer,
-  users: fetchUsersReducer,
+  users: UsersReducer,
   roles: RolesReducer,
   createRoles
   
